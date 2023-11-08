fd6d9e4617feb26979e2888d09d78135692fe4c9
rinkb <rinkb@students.uni-marburg.de>
2023-11-08 16:06:01 +0100

    first steps statisticsTrain.py

--- statisticsTrain.py
+++ statisticsTrain.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 
 # Read the data
 data = pd.read_csv('train.csv')
+
+# Setting to display all columns
+pd.set_option('display.width', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+# check all columns for missing entries without shortening the output
+print(data.isnull().sum())
