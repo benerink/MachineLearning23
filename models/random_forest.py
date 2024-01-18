6e801ac4e29c2cc77d4350795760cc69c6066eeb
Luca <schulz.l.luca@gmail.com>
2024-01-18 03:07:22 +0100

    -new model

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
@@ -56,6 +57,10 @@ def train_and_predict_random_forest(df, top_features):
     # Extract features and target variable
     X = df[features]
     y = df['Verkaufspreis']
+
+    # Handle missing values (NaN) or zeros by imputing the mean value
+    imputer = SimpleImputer(strategy='mean')
+    X_imputed = imputer.fit_transform(X)
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
